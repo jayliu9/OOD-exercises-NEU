560845b37883f348c83fed3031011f077b46634e
liushijie <liu.shijie@northeastern.edu>
2021-01-21 10:12:53 +0800

    Author class finish

---
+++ Labs/lab1/src/test/java/AuthorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception{
+    }
+
+    @Test
+    public void getEmail() throws Exception{
+    }
+
+    @Test
+    public void getAddress() throws Exception{
+    }
+}

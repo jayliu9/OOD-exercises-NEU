85d1408b2763da3fb94a81267b4b6413fa7b4aee
liushijie <liu.shijie@northeastern.edu>
2021-05-01 14:23:32 +0800

    lab9 problem1 finished

---
+++ Labs/lab9/src/main/java/problem1/TrendingTopics.java
@@ -0,0 +1,25 @@
+package problem1;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Represents the counter of topics
+ */
+public class TrendingTopics {
+
+  /**
+   * Gets the number of occurrences of every String in the input list as a Map, where every distinct
+   * String represents the Map key, and the number of the String’s occurrences the Map value.
+   *
+   * @param topicsList The list of Strings
+   * @return The number of occurrences of every String in the input list as a Map
+   */
+  public Map<String, Long> countTopics(List<String> topicsList) {
+    return topicsList.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+  }
+
+}
